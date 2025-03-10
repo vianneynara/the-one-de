943e3519bcc0e5b83869edf06548c1faee88406c
narwa <narwandaru.nara@gmail.com>
2025-03-10 23:59:51 +0700

    refactor: elaborate code instruction logic

--- src/routing/SprayAndWaitDERouter.java
+++ src/routing/SprayAndWaitDERouter.java
@@ -9,25 +9,31 @@ import core.Settings;
  * Decision Engine implementation for Spray And Wait algorithm.
  *
  * @author narwa
- * */
+ */
 public class SprayAndWaitDERouter implements RoutingDecisionEngine {
-	/** identifier for the initial number of copies setting ({@value})*/
+	/**
+	 * identifier for the initial number of copies setting ({@value})
+	 */
 	public static final String NROF_COPIES = "nrofCopies";
-	/** identifier for the binary-mode setting ({@value})*/
+	/**
+	 * identifier for the binary-mode setting ({@value})
+	 */
 	public static final String BINARY_MODE = "binaryMode";
 	/**
 	 * SprayAndWait router's settings name space ({@value})
-	 * */
+	 */
 	public static final String SPRAYANDWAIT_NS = "SprayAndWaitDERouter";
-	/** Message property key */
-	public static final String MSG_COUNT_PROPERTY = SPRAYANDWAIT_NS + "." +	"copies";
+	/**
+	 * Message property key
+	 */
+	public static final String MSG_COUNT_PROPERTY = SPRAYANDWAIT_NS + "." + "copies";
 
 	protected int initialNrofCopies;
 	protected boolean isBinary;
 
 	/**
 	 * Settings constructor.
-	 * */
+	 */
 	public SprayAndWaitDERouter(Settings s) {
 		if (s == null) {
 			s = new Settings(SPRAYANDWAIT_NS);
@@ -59,10 +65,10 @@ public class SprayAndWaitDERouter implements RoutingDecisionEngine {
 
 	/**
 	 * What is executed/processed before deciding whether a message should be forwarded on.
-	 * */
+	 */
 	@Override
 	public boolean newMessage(Message m) {
-		// adding spray and wait spray size
+		// adding spray and wait spray special property
 		m.addProperty(MSG_COUNT_PROPERTY, initialNrofCopies);
 
 		// message creation is always allowed
@@ -70,8 +76,10 @@ public class SprayAndWaitDERouter implements RoutingDecisionEngine {
 	}
 
 	/**
+	 * <b>(Sender side)</b>
+	 * From this current router, the message is in the process of being passed on.
 	 * The rule that checks whether a host qualifies as the final destination.
-	 * */
+	 */
 	@Override
 	public boolean isFinalDest(Message m, DTNHost otherHost) {
 //		if (otherHost.equals(m.getTo()))
@@ -82,33 +90,40 @@ public class SprayAndWaitDERouter implements RoutingDecisionEngine {
 	}
 
 	/**
+	 * <b>(Receiver side)</b>
 	 * Called when receiving a message from a peer to determine whether to be saved.
 	 *
 	 * @return True if the message should be saved, false otherwise.
-	 * */
+	 */
 	@Override
 	public boolean shouldSaveReceivedMessage(Message m, DTNHost thisHost) {
-		// Upon receiving the message, split it//decrement it
-//		System.out.printf("shouldSaveReceivedMessage: %s, has copies of %d\n", m.toString(), (int) m.getProperty(MSG_COUNT_PROPERTY));
-		int nrofCopies = (int) m.getProperty(MSG_COUNT_PROPERTY);
-		if (isBinary) {
-			nrofCopies = (int) Math.ceil(nrofCopies / 2.0);
-			m.updateProperty(MSG_COUNT_PROPERTY, nrofCopies);
-		} else {
-			m.updateProperty(MSG_COUNT_PROPERTY, --nrofCopies);
+		// this host which receives the message is not the destination thus,
+		// saving this message to be further routed.
+		if (!m.getTo().equals(thisHost)) {
+			// Upon receiving the message, split it//decrement it
+//			System.out.printf("shouldSaveReceivedMessage: %s, has copies of %d\n", m.toString(), (int) m.getProperty(MSG_COUNT_PROPERTY));
+			int nrofCopies = (int) m.getProperty(MSG_COUNT_PROPERTY);
+			if (isBinary) {
+				// use ceil (upper bound) on the receiving end
+				nrofCopies = (int) Math.ceil(nrofCopies / 2.0);
+				m.updateProperty(MSG_COUNT_PROPERTY, nrofCopies);
+			} else {
+				m.updateProperty(MSG_COUNT_PROPERTY, --nrofCopies);
+			}
+//			System.out.printf("shouldSaveReceivedMessage: %s, has copies of %d\n", m.toString(), (int) m.getProperty(MSG_COUNT_PROPERTY));
+			return true;
 		}
-//		System.out.printf("shouldSaveReceivedMessage: %s, has copies of %d\n", m.toString(), (int) m.getProperty(MSG_COUNT_PROPERTY));
 
-		// saves received message when the destination is not this current host
-		return !m.getTo().equals(thisHost);
+		return false;
 	}
 
 	/**
+	 * <b>(Sender side)</b>
 	 * Called when this router exchanges connection with a peer, to decide whether the message
 	 * should be passed or not.
 	 *
 	 * @return True if the message should be forwarded, false otherwise.
-	 * */
+	 */
 	@Override
 	public boolean shouldSendMessageToHost(Message m, DTNHost otherHost, DTNHost thisHost) {
 		// why would we need to forward the message if thisHost is the destination?
@@ -121,42 +136,53 @@ public class SprayAndWaitDERouter implements RoutingDecisionEngine {
 			return true;
 		}
 
-		return (int) m.getProperty(MSG_COUNT_PROPERTY) > 1 && otherHost != null;
+		// the message still has remaining copies left
+		return ((int) m.getProperty(MSG_COUNT_PROPERTY) > 1) && (otherHost != null);
 	}
 
 	/**
+	 * <b>(Sender side)</b>
 	 * Called after a message is sent to other peer, this determines whether the current
 	 * message should be deleted from the current host.
 	 *
 	 * @return True if the message should be deleted from the host, false otherwise.
-	 * */
+	 */
 	@Override
 	public boolean shouldDeleteSentMessage(Message m, DTNHost otherHost) {
 		// gather determining property
 		int nrofCopies = (int) m.getProperty(MSG_COUNT_PROPERTY);
 
-		// message does not have any more copies to share, delete it
+		// message does not have any more copies to share, delete it.
+		// this may indicate that the message has been received by its destination
 		if (nrofCopies <= 1) {
 			return true;
 		}
 
-		// check if the current SnW uses binary mode
+		// check if the current SnW uses binary mode to decrement
 		if (isBinary) {
+			// use floor (lower bound) on the sending end
 			nrofCopies = (int) Math.floor(nrofCopies / 2.0);
 		} else {
 			nrofCopies--;
 		}
 
 		m.updateProperty(MSG_COUNT_PROPERTY, nrofCopies);
+
+		// don't delete the message, it has not found the destination yet
 		return false;
 	}
 
 	/**
-	 * Deletes old message when the TTL has expired or the destination host told the message has expired/is old.
-	 * */
+	 * <b>(Sender side)</b>
+	 * Called if an attempt to transfer the message was unsuccessful OR already delivered to a peer.
+	 * Deletes old message when the TTL has expired, message property has reached zerp,
+	 * or the destination host told the message has expired/is old.
+	 *
+	 * @return True if the old message should be deleted, false otherwise.
+	 */
 	@Override
 	public boolean shouldDeleteOldMessage(Message m, DTNHost hostReportingOld) {
-		return m.getTtl() < 1 || m.getTo().equals(hostReportingOld);
+		return m.getTtl() < 1 || (int) m.getProperty(MSG_COUNT_PROPERTY) < 1 || m.getTo().equals(hostReportingOld);
 	}
 
 	@Override
@@ -171,7 +197,10 @@ public class SprayAndWaitDERouter implements RoutingDecisionEngine {
 
 	/**
 	 * Helper method to get another {@link SprayAndWaitDERouter} from another host.
-	 * */
+	 * This replicates Bryan's version of the method when trying to directly access other router.
+	 * We don't need it here.
+	 */
+	@Deprecated
 	private SprayAndWaitDERouter getDecisionEngineRouter(DTNHost otherHost) {
 		MessageRouter otherRouter = otherHost.getRouter();
 
