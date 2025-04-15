0924eda48e1e593056f09f67d4250926d6a0d2c7
narwa <narwandaru.nara@gmail.com>
2025-04-15 23:56:30 +0700

    docs: explanations

--- src/routing/community/CentralityCount.java
+++ src/routing/community/CentralityCount.java
@@ -6,7 +6,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Additional interface to support {@link report.PeriodicCommunityUniquesReporter}.
+ *
+ * @author narwa
+ * */
 public interface CentralityCount {
-
+	/**
+	 * Returns a sequential list of each encounter period/window given a connection history of a host.
+	 *
+	 * @param connHistory host's connection history.
+	 * @return List of unique encounters.
+	 * */
 	List<Set<DTNHost>> getGlobalEncounters(Map<DTNHost, List<Duration>> connHistory);
 }
